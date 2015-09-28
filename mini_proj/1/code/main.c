7b4920d4b08f5ee315081a58417b6a3ed78633c8
bernardo5 <bdnabaisgomes@yahoo.com>
2015-09-28 19:43:20 +0100

    any file can be oppened now

--- mini_proj/1/code/main.c
+++ mini_proj/1/code/main.c
@@ -1,14 +1,21 @@
 #include "file.h"
 
-int main(){
+int main(int argc, char *argv[]){
+	
+	if(argc!=2){
+		printf("Specify the file name\n");
+		exit(-1);
+	}
+	
+	char *table_txt=argv[1];
 	
 	char *prefix=malloc(10*sizeof(char));
 	int next_hop;
 	FILE*fp;
 	
-	fp = fopen( "teste_enunciado.txt" , "r");
+	fp = fopen( table_txt , "r");
 	if ( fp == NULL ) {
-		fprintf ( stderr, "ERRO! nao e possivel abrir o ficheiro: %s!\n", "teste_enunciado.txt");
+		fprintf ( stderr, "Error, cannot open file: %s!\n", table_txt);
 		exit ( 1 );
     }
 	

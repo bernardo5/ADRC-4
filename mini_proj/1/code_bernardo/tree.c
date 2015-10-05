8c120aaa1b219cbf387b1668f536f945ff5b5eef
bernardo5 <bdnabaisgomes@yahoo.com>
2015-10-05 20:49:24 +0100

    comentarios no tree.c

--- mini_proj/1/code_bernardo/tree.c
+++ mini_proj/1/code_bernardo/tree.c
@@ -1,9 +1,18 @@
 #include "tree.h"
 
+/*This function has the purpose of initializing the pointer that will be
+*the root of the tree*/
+
 node*Init_tree(){
 	node*aux=NULL;
 	return aux;
 }
+
+/*This function has the purpose of creating and initializing
+*a new node.
+*this might happen when inserting a new prefix, converting a forwarding
+*table into a binary prefix tree representation or in a convertion from binary prefix
+*prefix tree into a binary prefix 2-tree*/
 
 node* Initialize_node(char*prefix){
 	node*n=malloc(sizeof(node));
@@ -15,80 +24,81 @@ node* Initialize_node(char*prefix){
 	return n;
 }
 
+/*Function that adds a new prefix into a binary prefix tree*/
+
 void AddPrefix(node**root, char*prefix, int next_hop){
 	/*the prefix read can be * for empty prefixes
 		 * or a bit sequence*/
+
+		 /*this variable will be used to store the path already covered
+		 *it will be copied into a node structure, to make the table printing easier*/
 		char* current_prefix=malloc(sizeof(char)*(strlen(prefix)+1));
-		
+        /*if the prefix specified is the root some storage information has no use
+        *for ex:prefix(has none), no need of covering a tree path, etc*/
 		if(strcmp(prefix, "*")==0){
 			(*root)->next_hop=next_hop;
 			(*root)->prefix=malloc(sizeof(char)*strlen(prefix)+1);
 			strcpy((*root)->prefix, prefix);
 		}else{
-			int bit=0;
-			node*auxiliar=(*root);
-			while(bit<strlen(prefix)){
+		    /*in this point, we have the necessity of covering a tree path which will
+		    *depend on the prefix given*/
+			int bit=0; /*will define which prefix bit we should analyze*/
+			node*auxiliar=(*root); /*auxiliar pointer that will go through the tree*/
+			while(bit<strlen(prefix)){ /*while we havent reached th end of the path specified by the prefix*/
 				if(prefix[bit]=='0'){
 					strcat(current_prefix, "0");
-					if((auxiliar->zero)==NULL){
-						if(next_hop!=-1){
-							(auxiliar->zero)=Initialize_node(current_prefix);
-						}else{
-							printf("Prefix does not exist\n");
-							return;
-						}
-					}
+					if((auxiliar->zero)==NULL){/*if the node doesnt exist its necessary to create it*/
+						(auxiliar->zero)=Initialize_node(current_prefix);/*node creation with the prefix specification*/
+                    }
 					auxiliar=auxiliar->zero;
 				}else{
 					strcat(current_prefix, "1");
 					if((auxiliar->one)==NULL){
-						if(next_hop!=-1){
-							(auxiliar->one)=Initialize_node(current_prefix);
-						}else{
-							printf("Prefix does not exist\n");
-							return;
-						}
-					}	
+                        (auxiliar->one)=Initialize_node(current_prefix);
+                    }
 					auxiliar=auxiliar->one;
 				}
 				bit++;
 			}
+			/*copying the prefix values into the node*/
 			auxiliar->next_hop=next_hop;
 			auxiliar->prefix=malloc(sizeof(char)*strlen(prefix)+1);
 			strcpy(auxiliar->prefix, prefix);
 		}
-	free(current_prefix);
+	free(current_prefix);/*free of the auxiliar string used*/
 	return;
 }
+
+/*function that frees a certain node*/
 
 void Free_Node(node*base_node){
-	
+
 	free(base_node->prefix);
 	free(base_node);
-	
+
 	return;
 }
 
+/*function that updates the prefix string "deleting" what has already been processed*/
+
 char*NewPrefix(char*prefix){
-	
+
 	prefix++;
-	
+
 	return prefix;
 }
 
 int DeletePrefix(node**base_node, char*prefix){
-	
-	/*AddPrefix(root, prefix, -1); versao apagar sem frees*/
-	
+    /*its not supposed to delete the root*/
 	if(strcmp("*", prefix)==0) return -1;
-	
+    /*if we havent reached the end of the string we havent reached the node we wanted to delete*/
 	if(strlen(prefix)>0){
-	
 		if(prefix[0]=='0'){
 			prefix=NewPrefix(prefix);
-			/*verificar que existe no*/
+			/*verify if the node exists*/
 			if((*base_node)->zero!=NULL){
 				if((DeletePrefix((&(*base_node)->zero), prefix))==1){
+                    /*if the child has been deleted the pointer is now NULL*/
 					(*base_node)->zero=NULL;
 				}
 			}
@@ -96,9 +106,6 @@ int DeletePrefix(node**base_node, char*prefix){
 				printf("Prefix doesn't exist\n");
 				return -1;
 			}
-			
-			
-			
 		}else if(prefix[0]=='1'){
 			prefix=NewPrefix(prefix);
 			if((*base_node)->one!=NULL){
@@ -111,28 +118,30 @@ int DeletePrefix(node**base_node, char*prefix){
 			}
 		}
 	}
-	
-	
+
+
 	else if(strlen(prefix)==0){
-	/*chegamos ao destino*/
+	/*we reached the node to be deleted*/
 		if((*base_node)->zero==NULL && (*base_node)->one==NULL){
+			/*the node has no children so we can free its memory*/
 			Free_Node(*base_node);
 			return 1;
 		}else{
+		    /*the node has at least a child so we just say it has no next hop*/
 			(*base_node)->next_hop=-1;
-		}	
+		}
 	}
-	
+
 	return 0;
 }
 
 
 void ReadTable(node**root, char*table_txt){
-	
+    /*its said that we can assume addresses have 8 bits rather than 32*/
 	char *prefix=malloc(10*sizeof(char));
 	int next_hop;
 	FILE*fp;
-	
+
 	/*file opening*/
 	fp = fopen( table_txt , "r");
 	if ( fp == NULL ) {
@@ -141,9 +150,9 @@ void ReadTable(node**root, char*table_txt){
     }
 
 	/*Tree initialization with the root pointer*/
-	
-	(*root) = Initialize_node("*");	
-	
+
+	(*root) = Initialize_node("*");
+    /*while the file has info, we will had it to the tree*/
 	while(get_table_line(&prefix, &next_hop, fp)==0){
 		AddPrefix(root, prefix, next_hop);
 	}
@@ -152,13 +161,13 @@ void ReadTable(node**root, char*table_txt){
 }
 
 void PrintTable(node*base_node){
-	if((base_node->next_hop)!=-1){
+	if((base_node->next_hop)!=-1){/*the prefix of the node visited has a next hop info*/
 		printf("%s\t%d\n", base_node->prefix, base_node->next_hop);
 	}
-	if(base_node->zero!=NULL){
+	if(base_node->zero!=NULL){/*if it has a child this way, analyses its sub-tree*/
 		PrintTable(base_node->zero);
 	}
-	if(base_node->one!=NULL){
+	if(base_node->one!=NULL){/*if it has a child this way, analyses its sub-tree*/
 		PrintTable(base_node->one);
 	}
 	return;

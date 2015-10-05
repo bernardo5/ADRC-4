710958c8de0fb9c645540059278fa06eb0002ceb
bernardo5 <bdnabaisgomes@yahoo.com>
2015-10-05 20:06:05 +0100

    comentario na funcao

--- mini_proj/1/code_bernardo/file.c
+++ mini_proj/1/code_bernardo/file.c
@@ -1,5 +1,8 @@
 #include "file.h"
 
+/* This function has the purpose to read a line from the file given and
+*store the prefix and next hop values into the respective variables*/
+
 int get_table_line(char**prefix, int*next_hop, FILE*fp){
 	if(fscanf(fp, "%s %d", *prefix, next_hop)!=2){
 		return -1;

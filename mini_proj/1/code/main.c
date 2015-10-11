c94d8f1ab87cd77550daf47b01199fca67c00d1e
bernardo5 <bdnabaisgomes@yahoo.com>
2015-10-11 19:35:47 +0100

    supostamente final->p testar

--- mini_proj/1/code/main.c
+++ mini_proj/1/code/main.c
@@ -1,28 +1,28 @@
 #include "two_tree.h"
 
 int main(int argc, char *argv[]){
-	
+
 	if(argc<2){
 		printf("Specify the file name\n");
 		exit(-1);
 	}
-	
+
 	char*table_txt=argv[1];
-	
+
 	node*root=Init_tree();
-	
+
 	if(root==NULL)printf("Table successfully initialized\n");
-	
+
 	ReadTable(&root, table_txt);
-	
+
 	int option;
 	int hop;
 	char prefix[128];
-	
+
 	while(1){
-		
+
 		printf("Choose from the following options:\n1: Add Prefix\n2: Delete Prefix\n3: Print Table\n4: Convert Tree to Two Tree\n5: Address Look Up\n6: Exit\n\n");
-		
+
 		bzero(prefix, 128);
 		scanf("%d",&option);
 
@@ -41,10 +41,10 @@ int main(int argc, char *argv[]){
 		}
 		if(option==3){
 			PrintTable(root);
-			printf("\nTable successfully printed\n");	
+			printf("\nTable successfully printed\n");
 		}
 		if(option==4){
-			convert_tree(&root, root->next_hop);
+			TwoTree(&root, root->next_hop);
 			printf("\nTable successfully converted\n");
 		}
 		if(option==5){

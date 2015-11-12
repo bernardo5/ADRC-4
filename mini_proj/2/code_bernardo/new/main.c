270c8694cd130b09df316e4ad9c75f4ae369545f
bernardo5 <bdnabaisgomes@yahoo.com>
2015-11-12 13:22:24 +0000

    final

--- mini_proj/2/code_bernardo/new/main.c
+++ mini_proj/2/code_bernardo/new/main.c
@@ -20,23 +20,23 @@ int main(int argc, char**argv){
 		stat_hops[be]=0;
 	}
 	
-	float ones=0, twos=0, threes=0;
-	int colum;
+	float ones=0, twos=0, threes=0, unusable=0;
+	int colum=0;
 	for(colum=0; colum<size; colum++){
 		if((list[colum].link)!=NULL){ /*node exists*/
 			Dijkstra(list, colum+1, size, &node_distance, &node_hops);
-			/*for(be=0; be<size; be++){
+			printf("finished one dijkstra for node %d\n", colum+1);
+			for(be=0; be<size; be++){
 				if((list[be].link)!=NULL){
 					printf("%d\t%d\t%d\n", be+1, (node_distance)[be], (node_hops)[be]);
 				}
-			}*/
-			paths_count(size, node_distance, &ones, &twos, &threes);
+			}
+			paths_count(size, node_distance, &ones, &twos, &threes, &unusable);
 			hops_count(&stat_hops, node_hops, size);
-			//printf("finished one dijkstra\n");
 		}
 	}
 	/*sem heap*/
-	paths_statistics(&stat_customer, &stat_peer, &stat_provider, count_number_nodes(list, size), ones, twos, threes);
+	paths_statistics(&stat_customer, &stat_peer, &stat_provider, count_number_nodes(list, size), ones, twos, threes, unusable);
 	
 	printf("customer: %f\nstat_peer:%f\nstat_provider:%f\n\n", stat_customer, stat_peer, stat_provider);
 	
--- mini_proj/2/code_bernardo/new/statistics.c
+++ mini_proj/2/code_bernardo/new/statistics.c
@@ -1,7 +1,7 @@
 #include "statistics.h"
 
 
-void paths_count(int count_nodes, int*node_distance, float *ones, float* twos, float *threes){
+void paths_count(int count_nodes, int*node_distance, float *ones, float* twos, float *threes, float*unusable){
 	int i;
 		
 	for(i=0; i<count_nodes; i++){
@@ -14,7 +14,9 @@ void paths_count(int count_nodes, int*node_distance, float *ones, float* twos, f
 				if(node_distance[i]==3){
 					(*threes)++;
 				}else{
-					/*no path*/
+					if(node_distance[i]==-1){
+						(*unusable)++;
+					}
 				}
 			}
 		}
@@ -47,11 +49,11 @@ int count_number_nodes(node*list, int size){
 }
 
 
-void paths_statistics(float *stat_customer, float *stat_peer, float *stat_provider, int count_nodes, float ones, float twos, float threes){
-	(*stat_customer)=ones/((count_nodes-1)*count_nodes);
-	(*stat_peer)=twos/((count_nodes-1)*count_nodes);
-	(*stat_provider)=threes/((count_nodes-1)*count_nodes);
-	printf("Unusable: %f\n", 1-(*stat_customer)-(*stat_peer)-(*stat_provider));
+void paths_statistics(float *stat_customer, float *stat_peer, float *stat_provider, int count_nodes, float ones, float twos, float threes, float unusable){
+	(*stat_customer)=ones/(ones+twos+threes+unusable/*(count_nodes-1)*count_nodes*/);
+	(*stat_peer)=twos/(ones+twos+threes+unusable/*(count_nodes-1)*count_nodes*/);
+	(*stat_provider)=threes/(ones+twos+threes+unusable/*(count_nodes-1)*count_nodes*/);
+	printf("Unusable: %f\n", unusable/(ones+twos+threes+unusable/*(count_nodes-1)*count_nodes*/));
 	return;
 }
 

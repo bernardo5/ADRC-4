e787d43efaa1d4efba9cf27a0d87bf9da8d7e772
bernardo5 <bdnabaisgomes@yahoo.com>
2015-11-13 18:19:43 +0000

    ordenacao com hops

--- mini_proj/2/code_bernardo/new/dijkstra.c
+++ mini_proj/2/code_bernardo/new/dijkstra.c
@@ -19,7 +19,7 @@ void Initialize_distance_matrix(int count_nodes, int**node_distance, int**node_h
 			(*node_hops)[i]=0;
 		}
 		if((list[i].link)!=NULL) /*the node exists*/
-			Insert(h, i+1, (*node_distance), &(*heap_place));
+			Insert(h, i+1, (*node_distance), &(*heap_place), (*node_hops));
 	}	
 	return;
 }
@@ -49,7 +49,7 @@ void Dijkstra(node*list, int destiny, int count_nodes, int**node_distance, int**
 		heap=NewHeap(count_number_nodes(list, count_nodes));
 		Initialize_distance_matrix(count_nodes, &(*node_distance), &(*node_hops), list, destiny, heap, &heap_place);
 		while(HeapEmpty(heap)){
-			dijkstra_identifier= RemoveMax(heap, (*node_distance), &heap_place);
+			dijkstra_identifier= RemoveMax(heap, (*node_distance), &heap_place, (*node_hops));
 			//printf("removed %d from heap\n", dijkstra_identifier);
 			dijkstra_u = dijkstra_identifier - 1;
 			if((*node_distance)[dijkstra_u]!=-1){
@@ -71,8 +71,9 @@ void Dijkstra(node*list, int destiny, int count_nodes, int**node_distance, int**
 										//these 2 lines were outside the else
 										(*node_distance)[(links->identifier)-1] = min((*node_distance)[dijkstra_u],links->preference);
 							
-										FixUp(heap, heap_place[(links->identifier)-1], (*node_distance), &heap_place);
-									}					
+										
+									}
+									FixUp(heap, heap_place[(links->identifier)-1], (*node_distance), &heap_place, (*node_hops));					
 									
 								}
 							}		
--- mini_proj/2/code_bernardo/new/heap.h
+++ mini_proj/2/code_bernardo/new/heap.h
@@ -29,11 +29,11 @@ Heap *NewHeap(int size);
 
 /*void FreeHeap(Heap * h);*/
 
-void Insert(Heap * h, int element, int*node_distance, int**heap_place);
+void Insert(Heap * h, int element, int*node_distance, int**heap_place, int*node_hops);
 
-void FixUp(Heap * h, int k, int *node_distance, int**heap_place);
+void FixUp(Heap * h, int k, int *node_distance, int**heap_place, int*node_hops);
 
-int RemoveMax(Heap * h, int *node_distance, int**heap_place);
+int RemoveMax(Heap * h, int *node_distance, int**heap_place, int*node_hops);
 
 int PrintHeap(Heap * h);
 

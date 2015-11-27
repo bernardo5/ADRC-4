6cfd1adb548092b260fa6422c52dae059b15ebe3
bernardo5 <bdnabaisgomes@yahoo.com>
2015-11-27 16:17:54 +0000

    fifo functions

--- mini_proj/3/code_bernardo/file.c
+++ mini_proj/3/code_bernardo/file.c
@@ -38,22 +38,38 @@ void list_adj(node**list, int initial_node, int final_node){
 	return;
 }
 
-void Read_file(char * ficheiroIn, node**list){
+int Read_file(char * ficheiroIn, node**list){
 	
 	FILE*fp;
 	int initial_node;
 	int final_node;
-
+	int size=0;
+	
+	/*****************************************************************/
+	/*getting number of nodes*/
 	fp = fopen( ficheiroIn , "r");
 	if ( fp == NULL ) {
 		fprintf ( stderr, "ERROR! Cannot open file: %s!\n", ficheiroIn);
 		exit ( 1 );
     }
-    (*list)=malloc((100)*sizeof(node));
+    
+     while(get_table_line(&initial_node, &final_node, fp)==0){
+		if(size<initial_node) size=initial_node;
+	}
+    
+    fclose(fp);
+    size=size+1;
+	/*********************************************************************/
+	fp = fopen( ficheiroIn , "r");
+	if ( fp == NULL ) {
+		fprintf ( stderr, "ERROR! Cannot open file: %s!\n", ficheiroIn);
+		exit ( 1 );
+    }
+    (*list)=malloc((size)*sizeof(node));
     if((*list)==NULL)printf("ERROR IN MALLOC\n");
 	
 	int i=0;
-	for(i=0; i<100; i++){
+	for(i=0; i<size; i++){
 		create_link_entry_same(&(*list), i);
 		((*list)[i]).plus=NULL;
 	}
@@ -65,16 +81,16 @@ void Read_file(char * ficheiroIn, node**list){
 	
 	fclose(fp);
 	
-	/*adj_node * aux_adj_node;
+	adj_node * aux_adj_node;
 	
-	for(i=0; i<4; i++){
+	for(i=0; i<size; i++){
 		printf("\n node %d \n", i);
 		printf("\t minus: connected to %d with capacity %d\n", (((*list)[i]).minus)->identifier, (((*list)[i]).minus)->capacity);
 		for(aux_adj_node = ((*list)[i]).plus; aux_adj_node != NULL; aux_adj_node = aux_adj_node->next){
 			printf("\t plus: connected to %d with capacity %d\n", aux_adj_node->identifier, aux_adj_node->capacity);
 		}
 		printf("\n");
-	}*/
+	}
 	
-	return;
+	return size;
 }

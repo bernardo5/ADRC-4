89b3390dc751a2cf4fd604a00be5251691f34f10
bernardo5 <bdnabaisgomes@yahoo.com>
2015-11-28 19:55:44 +0000

    final parte 1

--- mini_proj/3/code_bernardo/main.c
+++ mini_proj/3/code_bernardo/main.c
@@ -1,5 +1,21 @@
 #include "ford-folkerson.h"
 
+int verify_join_nodes(node*list, int initial_node, int final_node){
+	int join=0;
+	adj_node*aux_adj_node;
+	for(aux_adj_node=((list)[initial_node]).plus; aux_adj_node!=NULL; aux_adj_node=aux_adj_node->next){
+		printf("identifier: %d\n", aux_adj_node->identifier);
+		if(aux_adj_node->identifier==final_node)join=1;
+	}
+	
+	for(aux_adj_node=((list)[final_node]).plus; aux_adj_node!=NULL; aux_adj_node=aux_adj_node->next){
+		if(aux_adj_node->identifier==initial_node)join=1;
+	}
+	
+	return join;
+}
+
+
 int main(int argc, char**argv){
 	char * ficheiroIn;
 	node * list;
@@ -27,6 +43,11 @@ int main(int argc, char**argv){
 		exit(-1);
 	}
 	
-	FordFulkerson(&list, size, &parent, initial_node, final_node);
+	if(verify_join_nodes(list, initial_node, final_node)){
+		printf("nodes are connected, cannot take a node between them to separate them\n");
+	}else{
+		FordFulkerson(&list, size, &parent, initial_node, final_node);
+	}
+	
 	exit(0);
 }
